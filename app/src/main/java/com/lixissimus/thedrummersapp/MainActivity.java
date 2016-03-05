852f57cb7f9fab98db99812c059dc45c01949dfe
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2016-03-05 14:16:40 +0100

    get rid of google indexing code, get rid of buttons, use activity's lifecycle for start and stop recording

--- app/src/main/java/com/lixissimus/thedrummersapp/MainActivity.java
+++ app/src/main/java/com/lixissimus/thedrummersapp/MainActivity.java
@@ -3,17 +3,12 @@ package com.lixissimus.thedrummersapp;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.Yin;
@@ -25,45 +20,33 @@ public class MainActivity extends AppCompatActivity {
     final int SAMPLING_RATE = 8000;
 
     private boolean recording = false;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button startButton = (Button) findViewById(R.id.startButton);
-        Button stopButton = (Button) findViewById(R.id.stopButton);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Stop recording due to onPause event");
+        stopRecording();
+    }
 
-        startButton.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        startRecording();
-                    }
-                }
-        );
-
-        stopButton.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        stopRecording();
-                    }
-                }
-        );
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Start recording due to onResume event");
+        startRecording();
     }
 
     private void startRecording() {
         if (recording) {
             return;
         }
+
         Log.i(TAG, "Start recording");
         recording = true;
 
@@ -71,8 +54,6 @@ public class MainActivity extends AppCompatActivity {
                 new Runnable() {
                     @Override
                     public void run() {
-                        Log.i(TAG, "micThread started...");
-
                         final int CHANNEL = AudioFormat.CHANNEL_IN_MONO;
                         final int FORMAT = AudioFormat.ENCODING_PCM_16BIT;
                         final int BUFFER_SIZE = AudioRecord.getMinBufferSize(
@@ -80,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
                         short[] buffer = new short[BUFFER_SIZE];
 
-                        Log.d(TAG, "Creating the AudioRecord");
                         AudioRecord recorder = new AudioRecord(
                                 MediaRecorder.AudioSource.MIC,
                                 SAMPLING_RATE,
@@ -89,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                                 BUFFER_SIZE * 10
                         );
 
-                        Log.d(TAG, "AudioRecord recording...");
                         recorder.startRecording();
 
                         while (recording) {
@@ -150,45 +129,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return fBuffer;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.lixissimus.thedrummersapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse(null),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.lixissimus.thedrummersapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
