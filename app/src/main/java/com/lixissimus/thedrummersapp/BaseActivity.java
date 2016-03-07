677821008606c1d48f08e02d25290caa68660d19
Lixissimus <felix.wolff@student.hpi.uni-potsdam.de>
2016-03-07 19:18:11 +0100

    handle drawer menu highlighting accordingly

--- app/src/main/java/com/lixissimus/thedrummersapp/BaseActivity.java
+++ app/src/main/java/com/lixissimus/thedrummersapp/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.lixissimus.thedrummersapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +14,37 @@ import android.view.MenuItem;
 public class BaseActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String MENU_INDEX_PARAM = "activeMenuItemId";
+
+    private NavigationView navigationView;
+    private int menuItemId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        menuItemId = -1;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // When back button is pressed to switch back to previous activity,
+        // we need to update the intent, otherwise it uses the one it got
+        // when the activity was created. That messes up the menu highlighting.
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            menuItemId = extras.getInt(BaseActivity.MENU_INDEX_PARAM);
+        }
+        setMenuItemChecked();
+    }
+
     protected void onCreateDrawer() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,8 +55,22 @@ public class BaseActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setMenuItemChecked();
+
+    }
+
+    private void setMenuItemChecked() {
+        if (menuItemId >= 0) {
+            navigationView.getMenu().findItem(menuItemId).setChecked(true);
+        } else {
+            // uncheck all items, since no id was provided
+            for (int i = 0; i < navigationView.getMenu().size(); i++) {
+                navigationView.getMenu().getItem(i).setChecked(false);
+            }
+        }
     }
 
     @Override
@@ -63,18 +109,23 @@ public class BaseActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_activity_metronome) {
-            Intent i = new Intent(this, MetronomeActivity.class);
-            startActivity(i);
-        } else if (id == R.id.nav_activity_tuner) {
-            Intent i = new Intent(this, DrumListActivity.class);
-            startActivity(i);
-        }
+        int itemId = item.getItemId();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+        if (itemId == R.id.nav_activity_metronome) {
+            Intent i = new Intent(this, MetronomeActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            i.putExtra(MENU_INDEX_PARAM, itemId);
+            startActivity(i);
+        } else if (itemId == R.id.nav_activity_tuner) {
+            Intent i = new Intent(this, DrumListActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            i.putExtra(MENU_INDEX_PARAM, itemId);
+            startActivity(i);
+        }
+
         return true;
     }
 }
